28f730fc249cbe7ace62e5a8a1fa69b9ab371662
Mick Deijs <341385@student.mboutrecht.nl>
2026-07-02 14:34:52 +0200

    database probleem opgelost

--- database/CreateScripts/behandelingen/sp_DeleteBehandeling.sql
+++ database/CreateScripts/behandelingen/sp_DeleteBehandeling.sql
@@ -6,6 +6,17 @@ CREATE PROCEDURE sp_DeleteBehandeling(
     IN p_id INT
 )
 BEGIN
+    -- Verwijder eerst alle gekoppelde gegevens
+    DELETE FROM Afspraak
+    WHERE BehandelingId = p_id;
+
+    DELETE FROM BehandelingProduct
+    WHERE BehandelingId = p_id;
+
+    DELETE FROM BehandelingSpecialisatie
+    WHERE BehandelingId = p_id;
+
+    -- Verwijder daarna de behandeling zelf
     DELETE FROM Behandeling
     WHERE BehandelingId = p_id;
 
